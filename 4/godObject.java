17c7f3f65c606c5f741864d79b35be9bd33e23f7
Jimmy Pulido <jiapulidoar@gmail.com>
2019-11-15 11:22:51 -0500

    Add p4 and complement README :sparkles:

---
+++ 4/godObject.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+
+
+// God object: performs a lot of functions which would be better separated into different objects.
+class Transport {
+    public void takeTransport();
+    public void DepartFrom() {}
+    public void ReadFromFile() {}
+    public void WriteToFile() {}
+    public void DisplayToScreen() {}
+    public void PerformCalculation() {}
+    public void Validatedestination() {}
+
+    public void Bus(){}
+    public void Bicicle(){}
+    public void Cap(){}
+
+}
